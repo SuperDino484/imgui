c340e029d2bdcd14eeb1e75918909ac47a610508
SuperDino484 <39782317+SuperDino484@users.noreply.github.com>
2021-12-10 21:01:13 -0700

    Created premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+        systemversion "latest"
+
+	filter {"system:windows", "configurations:Debug"}
+        buildoptions "/MTd"
+		runtime "Debug"
+
+	filter {"system:windows", "configurations:Release"}
+        buildoptions "/MT"
+		runtime "Release"
